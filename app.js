e2499cf44636bb135214d615d960c1f2861bfc9f
Vbeni <benivillafana@gmail.com>
2023-02-04 16:26:19 -0500

    <Commit 3 - Is a Vowel.>

--- app.js
+++ app.js
@@ -12,8 +12,13 @@ function calculateCube(num) {
 console.log(calculateCube(5));
 // 3. isAVowel
 function isAVowel(letter) {
-    // YOUR CODE HERE
-}
+    letter = letter.toLowerCase();
+    if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+    return true;
+    else return false; 
+
+};
+console.log(isAVowel('a'));
 
 // 4. getTwoLengths
 function getTwoLengths(word1, word2) {
